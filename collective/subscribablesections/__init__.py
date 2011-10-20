868fcc1f1d53e534e9bf10745b3d933bba529b05
Kees Hink <keeshink@gmail.com>
2011-10-20 08:31:46 +0000

    For debugging: check syntax of manage.py on startup

--- collective/subscribablesections/__init__.py
+++ collective/subscribablesections/__init__.py
@@ -3,6 +3,7 @@ from zope.i18nmessageid import MessageFactory
 import p4a.z2utils # Patch CMFDynamicViewFTI 
 
 MessageFactory = MessageFactory('collective.subscribablesections')
+import manager # XXX DEBUG Check syntax on startup
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
