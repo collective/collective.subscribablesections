f62fb17cd2c9a7747fb34ed93b8ff30c0453e33e
Franklin Kingma <franklin@fourdigits.nl>
2017-05-23 09:28:37 +0200

    Preparing release 1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '1.1'
 long_description = open("README.txt").read() + "\n" + \
                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
                    open(os.path.join("docs", "LICENSE.txt")).read() + "\n" + \
